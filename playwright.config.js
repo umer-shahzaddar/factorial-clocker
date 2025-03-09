eae28b0b83f6907559655a677ed77b9c48b39c30
umer <umer.shahzaddar@subbyx.com>
2025-03-09 01:28:59 +0100

    feat: lighten playwright

--- playwright.config.js
+++ playwright.config.js
@@ -17,15 +17,14 @@ module.exports = defineConfig({
   retries: 0,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'dot',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    screenshot: 'only-on-failure'
+    trace: process.env.CI ? 'retain-on-failure' : 'off'
   },
 
   /* Configure projects for major browsers */
